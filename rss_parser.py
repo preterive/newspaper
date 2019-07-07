6d5eeeffa651d1a88ac50ae3d2abd48183d7c816
Moritz Jäger <moritzjaeger@outlook.com>
2019-07-07 16:19:31 +0200

    feeds now get inserted into db

--- rss_parser.py
+++ rss_parser.py
@@ -3,11 +3,25 @@ import feedparser
 import requests
 
 import multiprocessing as mp
-import time
+from datetime import datetime
+from time import mktime
 import random
 import os
 from multiprocessing import Queue
+from feeds_db import *
 
+def time_struct_to_datetime(time_struct):
+    return datetime.fromtimestamp(mktime(time_struct))
+
+def datetime_converter(s):
+    try:
+        d = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z')
+    except:
+        d = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
+    if d.tzinfo != None:
+        p = d.strftime("%s")
+        d = datetime.utcfromtimestamp(float(p))
+    return d
 
 def download(url):
     #time.sleep(random.randint(1,3))
@@ -16,9 +30,7 @@ def download(url):
         raise Exception("Status code: "+str(r.status_code))
     print(url, r.status_code)
     feed = feedparser.parse(r.text)
-#    for item in feed.entries:
-#        q.put(item)
-    return feed
+    return url, feed
     
 processes = [ ]
 url_list = ['https://www.spiegel.de/schlagzeilen/index.rss',
@@ -27,34 +39,26 @@ url_list = ['https://www.spiegel.de/schlagzeilen/index.rss',
             'https://www.welt.de/feeds/latest.rss',
             'https://www.n-tv.de/rss']
 
+feeds = select_all_feeds()
+url_list = [i[2] for i in feeds]
+
+feed_ids = dict([(i[2],i[0]) for i in feeds])
+
+
 num_workers = mp.cpu_count()
+
 
 pool = mp.Pool(num_workers)
 q = Queue()
 feeds = pool.map(download, url_list)
-for i in feeds:
+
+entries = []
+for url, i in feeds:
     print(i.feed.title, len(i.entries))
-'''
-for url in url_list:
-    pool.apply_async(download, args = (url, q,))
-
-pool.close()
-pool.join()
-'''
-'''
-q = Queue()
-#for i in range(10):
-for i in url_list:
-    t = multiprocessing.Process(target=download, args=(i,q))
-    processes.append(t)
-    t.start()
-for process in processes:
-    process.join()
-
-mylist = [ ]
-while not q.empty():
-    rss = q.get()
-    mylist.append(rss)
-print("Done!")
-print(len(mylist))
-'''
+    for entry in i.entries:
+        print(entry.link)
+        e = (entry.title, entry.link, datetime.utcnow(),datetime_converter(entry.published),entry.summary, int(feed_ids[url]))
+        entries.append(e)
+    #insert_new_feed(url=url, title=i.feed.title, last_parsed=datetime.utcnow())
+    update_last_parsed(url, datetime.utcnow())
+insert_new_entries(entries)
