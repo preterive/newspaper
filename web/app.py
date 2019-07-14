bb93718660ac9ce763138294e2ff326baadccef7
Moritz Jäger <moritzjaeger@outlook.com>
2019-07-14 11:22:31 +0200

    displays all entries

--- web/app.py
+++ web/app.py
@@ -1,18 +1,25 @@
 import sqlite3
 from flask import Flask, render_template, request, g, redirect, Response
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
-DATABASE = '/Users/moritzjager/Desktop/Python/newspaper/feeds.db'
+db = '/Users/moritzjager/Desktop/Python/newspaper/feeds.db'
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    now = datetime.utcnow() + timedelta(0,300)
+    now = now.strftime("%Y/%m/%d %H:%M:%S")
+    t = g.db.execute('Select * from entries where published<=? order by published desc',(now,)).fetchall()
+    return render_template('index.html', entries=t)
 
 
 if __name__ == '__main__':
