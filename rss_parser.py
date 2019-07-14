cb497f6bed90e7c2152398b4bd58f405e3153e83
Moritz Jäger <moritzjaeger@outlook.com>
2019-07-14 11:20:55 +0200

    fixed datetime to utx parser

--- rss_parser.py
+++ rss_parser.py
@@ -4,6 +4,7 @@ import requests
 
 import multiprocessing as mp
 from datetime import datetime
+from dateutil.parser import parse
 from time import mktime
 import random
 import os
@@ -14,12 +15,14 @@ def time_struct_to_datetime(time_struct):
     return datetime.fromtimestamp(mktime(time_struct))
 
 def datetime_converter(s):
-    try:
-        d = datetime.strptime(s, '%a, %d %b %Y %H:%M:%S %z')
-    except:
-        d = datetime.strptime(s, '%a, %d %b %Y %H:%M:%S %Z')
+    d = parse(s)
+    n = d.strftime("%Y-%m-%d %H:%M:%S %z")
+    n = datetime.strptime(n, "%Y-%m-%d %H:%M:%S %z")
+    d = n
     if d.tzinfo != None:
+        print(s, ' - - ', d,d.tzinfo)
         p = d.strftime("%s")
+        print(p, '--', d.timestamp())
         d = datetime.utcfromtimestamp(float(p))
     return d
 
@@ -36,7 +39,8 @@ def opml_to_db():
 def download(url):
     r = requests.get(url)
     if r.status_code != 200:
-        raise Exception("Status code: "+str(r.status_code))
+        #raise Exception("Status code: "+str(r.status_code))
+        return None, None
     feed = feedparser.parse(r.text)
     return url, feed
 
@@ -50,27 +54,23 @@ def input_entries_into_db(feeds, url_feeds):
     feed_ids = dict([(i[2],i[0]) for i in url_feeds])
     entries = []
     for url, i in feeds:
-        print(i.feed.title, len(i.entries))
-        for entry in i.entries:
-            print(entry.link)
-            e = (entry.title, entry.link, datetime.utcnow(),datetime_converter(entry.published),entry.summary, int(feed_ids[url]))
-            entries.append(e)
-        update_last_parsed(url, datetime.utcnow())
-    insert_new_entries(entries)
+        if url != None:
+            print(i.feed.title, len(i.entries))
+            for entry in i.entries:
+                #print(entry.link)
+                try: 
+                    summary = entry.summary
+                except:
+                    summary = None
+                e = (entry.title, entry.link, datetime.utcnow(),datetime_converter(entry.published),summary, int(feed_ids[url]))
+                entries.append(e)
+            update_last_parsed(url, datetime.utcnow())
+        insert_new_entries(entries)
 
 def main():
     url_feeds = select_all_feeds()
     url_list = [i[2] for i in url_feeds]
     input_entries_into_db(download_multiple(url_list), url_feeds)
-
-    
-    
-url_list = ['https://www.spiegel.de/schlagzeilen/index.rss',
-            'https://www.tagesschau.de/xml/rss2',
-            'https://www.bild.de/rssfeeds/vw-alles/vw-alles-26970192,dzbildplus=false,sort=1,teaserbildmobil=false,view=rss2,wtmc=ob.feed.bild.xml',
-            'https://www.welt.de/feeds/latest.rss',
-            'https://www.n-tv.de/rss']
-
 
 if __name__ == '__main__':
     #opml_to_db()
