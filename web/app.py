156d7959467c8d8945b8963b188eb4b70c8b6ebc
Moritz Jäger <moritzjaeger@outlook.com>
2019-07-29 08:34:31 +0200

    now only returns 200 last entries in db

--- web/app.py
+++ web/app.py
@@ -19,7 +19,7 @@ def teardown_request(exception):
 def hello():
     now = datetime.utcnow() + timedelta(0,300)
     now = now.strftime("%Y/%m/%d %H:%M:%S")
-    t = g.db.execute('Select * from entries where published<=? order by published desc',(now,)).fetchall()
+    t = g.db.execute('Select * from entries where published<=? order by published desc limit 200',(now,)).fetchall()
     return render_template('index.html', entries=t)
 
 
