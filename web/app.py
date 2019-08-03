e33379890e5cca120bf7f8c1ef323effef028368
Moritz Jäger <moritzjaeger@outlook.com>
2019-08-03 09:59:55 +0200

    added doctype; removed src dayjs

--- web/app.py
+++ web/app.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import os
 from config import *
 from momentjs import momentjs
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -26,6 +27,7 @@ def hello():
     now = datetime.utcnow() + timedelta(0,300)
     now = now.strftime("%Y/%m/%d %H:%M:%S")
     t = g.db.execute('Select * from entries where published<=? order by published desc limit 200',(now,)).fetchall()
+    #t = [(i[0],i[1],i[2],i[3],i[4].astimezone(timezone('Europe/Berlin')),i[5],i[6]) for i in t]
     # TODO convert datetime to local de time, so that mometjs is not needed
     return render_template('index.html', entries=t)
 
