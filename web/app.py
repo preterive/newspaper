0c7ac879dae51a930dbdc127ad45ea708d957535
Moritz Jäger <moritz@keingenie.de>
2019-08-15 20:40:57 +0200

    removed basic security; to intrusive

--- web/app.py
+++ web/app.py
@@ -7,15 +7,10 @@ import config
 from momentjs import momentjs
 from pytz import timezone
 from flask_caching import Cache
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 
 c =  {
     "DEBUG":config.c_debug,
-    "SQLALCHEMY_DATABASE_URI":config.alchemy_uri,
-    "SECURITY_PASSWORD_SALT":config.pwd_salt,
-    "SECRET_KEY":config.secret_key,
     "CACHE_TYPE": "simple", # Flask-Caching related configs
     "CACHE_DEFAULT_TIMEOUT": 300
    }
@@ -25,36 +20,6 @@ app.config.from_mapping(c)
 cache = Cache(app)
 
 app.jinja_env.globals['momentjs'] = momentjs
-
-userdb = SQLAlchemy(app)
-
-# Define models
-roles_users = userdb.Table('roles_users',
-        userdb.Column('user_id', userdb.Integer(), userdb.ForeignKey('user.id')),
-        userdb.Column('role_id', userdb.Integer(), userdb.ForeignKey('role.id')))
-
-class Role(userdb.Model, RoleMixin):
-    id = userdb.Column(userdb.Integer(), primary_key=True)
-    name = userdb.Column(userdb.String(80), unique=True)
-    description = userdb.Column(userdb.String(255))
-
-class User(userdb.Model, UserMixin):
-    id = userdb.Column(userdb.Integer, primary_key=True)
-    email = userdb.Column(userdb.String(255), unique=True)
-    password = userdb.Column(userdb.String(255))
-    active = userdb.Column(userdb.Boolean())
-    confirmed_at = userdb.Column(userdb.DateTime())
-    roles = userdb.relationship('Role', secondary=roles_users,
-    backref=userdb.backref('users', lazy='dynamic'))
-
-user_datastore = SQLAlchemyUserDatastore(userdb, User, Role)
-security = Security(app, user_datastore)
-
-#@app.before_first_request
-def create_user():
-    userdb.create_all()
-    user_datastore.create_user(email='test@test.de', password='test')
-    userdb.session.commit()
 
 
 @app.before_request
@@ -85,7 +50,6 @@ def hello():
     return render_template('index.html', entries=t, feeds=get_feeds())
 
 @app.route("/add_feed")
-@login_required
 def add_feed():
     return render_template("feeds.html")
 
